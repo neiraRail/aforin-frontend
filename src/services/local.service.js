9428091200c428228e1fa8234976716ed690dfbb
michellenaour <54044891+michellenaour@users.noreply.github.com>
2021-11-28 15:37:23 -0300

    se agrega service

---
+++ src/services/local.service.js
@@ -0,0 +1,20 @@
+import ApiService from "@/services/api.service";
+const url = "local";
+export const service = {
+    getAll() {
+        return ApiService.get(url, { id: "" });
+    },
+    get(id) {
+        return ApiService.get(url, { slug: id });
+    },
+    create(params) {
+        return ApiService.post(url, { params: params });
+    },
+    update( params) {
+        return ApiService.update(url, { params: params });
+    },
+    destroy(id) {
+        return ApiService.delete(url, { id: id });
+    },
+};
+export default service;
