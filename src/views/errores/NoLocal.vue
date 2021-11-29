60c5427564df8c5f3810941409b42710fc0397c5
Rail Neira <neira.rail@gmail.com>
2021-11-28 23:28:29 -0300

    Ultimos cambios vista nolocal

--- src/views/errores/NoLocal.vue
+++ src/views/errores/NoLocal.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
-        <h1>No se encuentra tu id de local! :C</h1>
-        <p>
-            Por favor ve a tu pagina de administrador para revisar que la configuración esté correcta
-            o intentalo denuevo
-        </p>
-        <v-btn @click="$router.push('/')">Ir a Config</v-btn>
-        <v-btn @click="$router.push('/')">Intentar denuevo</v-btn>
+        <h1 align="center">No se encuentra tu id de local! :C</h1>
+        <div align="center">
+            <p align="center" style="width: 400px; text-align: justify;">
+                Por favor ve a tu pagina de administrador para revisar que la configuración esté correcta
+                o intentalo denuevo
+            </p>
+        </div>
+        <v-col align="center">
+            <v-btn @click="$router.push('/')">Ir a Config</v-btn>
+            <v-btn @click="$router.push('/')">Intentar denuevo</v-btn>
+        </v-col>
+        
     </div>
 </template>
 
